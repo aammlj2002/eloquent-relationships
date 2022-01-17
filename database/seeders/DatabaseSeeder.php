b61e65e4ad45a27f1a79e98b3067d2c7055d1d56
Leo <aammlj2002@gmail.com>
2022-01-17 14:34:11 +0630

    many to many relationship

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Role::factory(10)->create();
+        $roles=Role::all();
+        User::all()->each(function ($user) use ($roles) {
+            $user->roles()->saveMany($roles);
+        });
+        //store data
+        // $user->name = "mgmg";
+        // $user->save();
+        // $user->roles()->attach($roles);
     }
 }
