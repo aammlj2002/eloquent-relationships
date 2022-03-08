c6a2c00eda56417c43279f7b4da48df130f5426b
Leo <aammlj2002@gmail.com>
2022-03-08 17:55:57 +0630

    polymorphic many to many relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,13 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Video extends Model
+class Post extends Model
 {
     use HasFactory;
-    public function watchable()
-    {
-        return $this->morphTo();
-    }
     public function tags()
     {
         return $this->morphToMany(Tag::class, "taggable");
